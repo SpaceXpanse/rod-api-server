be8153f90b524ba16faedf1f46627306e0421900
Space Expanse <97041967+spaceexpanse@users.noreply.github.com>
2024-06-10 14:02:59 +0200

    Update general.py

--- server/methods/general.py
+++ server/methods/general.py
@@ -11,7 +11,7 @@ class General():
         if data["error"] is None:
             #data["result"]["supply"] = utils.supply(data["result"]["blocks"])["supply"]
             #data["result"]["reward"] = utils.reward2(data["result"]["blocks"])
-            data["result"]["supply"] = utils.supply(data["result"]["blocks"])["supply"]
+            #data["result"]["supply"] = utils.supply(data["result"]["blocks"])["supply"]
             data["result"]["reward"] = utils.reward2(data["result"]["blocks"])
             data["result"].pop("verificationprogress")
             data["result"].pop("initialblockdownload")
